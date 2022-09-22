f1391fa0c3329d11bb329285c2418d12ed5c1b33
HP <HP@10.12.8.224>
2022-09-22 15:20:24 +0530

    lastsort

---
+++ src/lastsort.java
@@ -0,0 +1,31 @@
+class lastsort {
+	void lastsort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+					// swap arr[j+1] and arr[j]
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	public static void main(String args[])
+	{
+		lastsort ob = new lastsort();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		ob.lastsort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
+}
